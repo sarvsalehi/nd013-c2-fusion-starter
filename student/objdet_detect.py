ce2018e9c396710ed2f42c299852de2721c952c9
Sarvenaz <Sarvenaz.salehi_mourkani@mercedes-benz.com>
2023-03-24 09:43:13 +0000

    Added step3 implementation

--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -14,6 +14,7 @@
 import numpy as np
 import torch
 from easydict import EasyDict as edict
+from tools.objdet_models.resnet.utils.torch_utils import _sigmoid
 
 # add project directory to python path to enable relative imports
 import os
@@ -61,7 +62,47 @@ def load_configs_model(model_name='darknet', configs=None):
         ####### ID_S3_EX1-3 START #######     
         #######
         print("student task ID_S3_EX1-3")
+        
+        configs.model_path = os.path.join(parent_path, 'tools', 'objdet_models', 'resnet')
+        configs.pretrained_filename = os.path.join(configs.model_path, 'pretrained', 'fpn_resnet_18_epoch_300.pth')
+        configs.arch = 'fpn_resnet_18'
+        configs.num_layers = 18
+        configs.conf_thresh = 0.5  # Used to find pkl
 
+        configs.pin_memory = True
+        configs.distributed = False
+
+        configs.input_size = (608, 608)
+        configs.hm_size = (152, 152)
+        configs.down_ratio = 4
+        configs.max_objects = 50
+
+        configs.imagenet_pretrained = False
+        configs.head_conv = 64
+        configs.num_classes = 3
+        configs.num_center_offset = 2
+        configs.num_z = 1
+        configs.num_dim = 3
+        configs.num_direction = 2  # sin, cos
+
+        configs.heads = {
+            'hm_cen': configs.num_classes,
+            'cen_offset': configs.num_center_offset,
+            'direction': configs.num_direction,
+            'z_coor': configs.num_z,
+            'dim': configs.num_dim
+        }
+
+        configs.num_input_features = 4
+
+        configs.K = 50
+        configs.num_samples = None
+        configs.num_workers = 1
+        configs.batch_size = 1
+        configs.peak_thresh = 0.2
+        configs.output_width = 608
+        configs.output_format = "image"
+        configs.output_video_fn = "out_fpn_resnet_18"
         #######
         ####### ID_S3_EX1-3 END #######     
 
@@ -119,6 +160,8 @@ def create_model(configs):
         #######
         print("student task ID_S3_EX1-4")
 
+        model = fpn_resnet.get_pose_net(num_layers=configs.num_layers, heads=configs.heads, head_conv=configs.head_conv,
+                                        imagenet_pretrained=configs.imagenet_pretrained)
         #######
         ####### ID_S3_EX1-4 END #######     
     
@@ -167,7 +210,14 @@ def detect_objects(input_bev_maps, model, configs):
             ####### ID_S3_EX1-5 START #######     
             #######
             print("student task ID_S3_EX1-5")
-
+            outputs['hm_cen'] = _sigmoid(outputs['hm_cen'])
+            outputs['cen_offset'] = _sigmoid(outputs['cen_offset'])
+            
+            detections = decode(outputs['hm_cen'], outputs['cen_offset'], outputs['direction'], outputs['z_coor'],
+                                outputs['dim'], K=configs.K)
+            detections = detections.cpu().numpy().astype(np.float32)
+            detections = post_processing(detections, configs)
+            detections = detections[0][1]
             #######
             ####### ID_S3_EX1-5 END #######     
 
@@ -180,12 +230,18 @@ def detect_objects(input_bev_maps, model, configs):
     objects = [] 
 
     ## step 1 : check whether there are any detections
-
+    if len(detections) > 0:
         ## step 2 : loop over all detections
-        
+         for obj in detections:
+                _, bev_x, bev_y, z, h, bev_w, bev_l, yaw = obj
             ## step 3 : perform the conversion using the limits for x, y and z set in the configs structure
+                x = (bev_y / configs.bev_height) * (configs.lim_x[1] - configs.lim_x[0]) + configs.lim_x[0]
+                y = (bev_x / configs.bev_width) * (configs.lim_y[1] - configs.lim_y[0]) + configs.lim_y[0]
+                w = (bev_w / configs.bev_width) * (configs.lim_y[1] - configs.lim_y[0]) 
+                l = (bev_l / configs.bev_height) * (configs.lim_x[1] - configs.lim_x[0])
         
             ## step 4 : append the current object to the 'objects' array
+                objects.append([1, x, y, z, h, w, l, -yaw])
         
     #######
     ####### ID_S3_EX2 START #######   
